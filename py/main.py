bfd34a230fb5acd009b2c30e34b312212c3c2e78
amFvcg <amfvcg@tutanota.com>
2016-03-31 00:39:40 +0200

    [PY] added validation and parsing + tests.

--- py/main.py
+++ py/main.py
@@ -1,20 +1,50 @@
+import json
 from collections import namedtuple
 
 Mapping = namedtuple('Mapping', ['name', 'type'])
 
 
 def mybool(val):
-    return True
+    return True if val in ['y', 'Y', '1']\
+        else False if val in ['n', 'N', '0']\
+        else None
 
 
-def do_mapping(lines):
-    return [Mapping(line.split('=')[0],
-                    int if line.split('=')[1] == 'int' else
-                    str if line.split('=')[1] == 'string' else
-                    mybool if line.split('=')[1] == 'bool' else None)
-            for line in lines]
+def do_mapping(line):
+    return Mapping(line.split('=')[0],
+                   int if line.split('=')[1] == 'int' else
+                   str if line.split('=')[1] == 'string' else
+                   mybool if line.split('=')[1] == 'bool' else None)
 
 
 def read_mapping(filename):
     with open(filename) as f:
-        return do_mapping(f)
+        return [do_mapping(line.strip()) for line in f if len(line.strip())]
+
+
+def validate_items(items, mapping):
+    if len(items) != len(mapping):
+        return False
+    try:
+        return all([True for i in
+                    map(lambda x, y: x.type(y), mapping, items)])
+    except ValueError:
+        return False
+
+
+def validate_file(filename, mapping):
+    with open(filename) as f:
+        return all([validate_items(line.split(';'), mapping) for line in f])
+
+
+def parse_items(items, mapping):
+    return dict([(key, value) for (key, value)
+                in map(lambda x, y: (x.name, x.type(y)), mapping, items)])
+
+
+def parse_file(filename, mapping):
+    with open(filename) as f:
+        return json.dumps(
+            [parse_items([item.strip() for item in line.split(';')
+                         if len(item.strip())], mapping)
+             for line in f])
--- py/tests/test_main.py
+++ py/tests/test_main.py
@@ -5,11 +5,42 @@ class TestMain(TestCase):
     def test_mybool(self):
         from main import mybool
         self.assertTrue(mybool("Y"))
+        self.assertTrue(mybool("y"))
+        self.assertTrue(mybool("1"))
+        self.assertFalse(mybool("N"))
+        self.assertFalse(mybool("n"))
+        self.assertFalse(mybool("0"))
 
     def test_do_mapping(self):
         from main import do_mapping, Mapping, mybool
         lines = ['id=int', 'name=string', 'mybool=bool']
+        parsed_lines = [do_mapping(line) for line in lines]
         self.assertListEqual([Mapping('id', int),
                               Mapping('name', str),
                               Mapping('mybool', mybool)],
-                             do_mapping(lines))
+                             parsed_lines)
+
+    def test_validate_items(self):
+        from main import validate_items, mybool, Mapping
+        items = ['1', 'some string', 'N']
+        mapping = [Mapping('id', int),
+                   Mapping('name', str),
+                   Mapping('mybool', mybool)]
+        self.assertTrue(validate_items(items, mapping))
+
+    def test_validate_items_fail(self):
+        from main import validate_items, mybool, Mapping
+        items = ['1a', 'some string', 'N']
+        mapping = [Mapping('id', int),
+                   Mapping('name', str),
+                   Mapping('mybool', mybool)]
+        self.assertFalse(validate_items(items, mapping))
+
+    def test_parse_items(self):
+        from main import parse_items, mybool, Mapping
+        items = ['1', 'some string', 'N']
+        mapping = [Mapping('id', int),
+                   Mapping('name', str),
+                   Mapping('mybool', mybool)]
+        self.assertDictEqual({'id': 1, 'name': 'some string', 'mybool': False},
+                             parse_items(items, mapping))
