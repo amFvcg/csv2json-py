12dd2b3e5121613fcfca275e10c8a64df2186849
amFvcg <amfvcg@tutanota.com>
2017-04-17 21:26:26 +0200

    Tests update

--- csv2json/mapping.py
+++ csv2json/mapping.py
@@ -6,13 +6,15 @@ def default(header):
     return [Mapping(name, str) for name in header]
 
 def do_mapping(line):
+    import csv2json.types as t
     name = line.split('=')[0]
     field_type = line.split('=')[1]
     return Mapping(name,
-                   int if field_type[0] == 'int' else
-                   str if line.split('=')[1] == 'string' else
-                   float if line.split('=')[1] == 'float' else
-                   mybool if line.split('=')[1] == 'bool' else None)
+                   int if field_type == 'int' else
+                   str if field_type == 'string' else
+                   float if field_type == 'float' else
+                   t.bool if field_type == 'bool' else
+                   None)
 
 
 def read_mapping(filename):
--- tests/test_main.py
+++ tests/test_main.py
@@ -2,45 +2,51 @@ from unittest import TestCase
 
 
 class TestMain(TestCase):
-    def test_mybool(self):
-        from csv2json.main import mybool
-        self.assertTrue(mybool("Y"))
-        self.assertTrue(mybool("y"))
-        self.assertTrue(mybool("1"))
-        self.assertFalse(mybool("N"))
-        self.assertFalse(mybool("n"))
-        self.assertFalse(mybool("0"))
-
     def test_do_mapping(self):
-        from csv2json.main import do_mapping, Mapping, mybool
-        lines = ['id=int', 'name=string', 'mybool=bool']
+        from csv2json.mapping import do_mapping, Mapping
+        from csv2json.types import bool
+        lines = ['id=int', 'name=string', 'bool=bool']
         parsed_lines = [do_mapping(line) for line in lines]
         self.assertListEqual([Mapping('id', int),
                               Mapping('name', str),
-                              Mapping('mybool', mybool)],
+                              Mapping('bool', bool)],
                              parsed_lines)
 
+    def test_default(self):
+        from csv2json.mapping import default
+        header = ['Field1', 'Field2']
+        self.assertListEqual([], [])
+
     def test_validate_items(self):
-        from csv2json.main import validate_items, mybool, Mapping
+        from csv2json.main import validate_items
+        from csv2json.mapping import Mapping
+        from csv2json.types import bool
+
         items = ['1', 'some string', 'N']
         mapping = [Mapping('id', int),
                    Mapping('name', str),
-                   Mapping('mybool', mybool)]
+                   Mapping('bool', bool)]
         self.assertTrue(validate_items(items, mapping))
 
     def test_validate_items_fail(self):
-        from csv2json.main import validate_items, mybool, Mapping
+        from csv2json.main import validate_items
+        from csv2json.mapping import Mapping
+        from csv2json.types import bool
+
         items = ['1a', 'some string', 'N']
         mapping = [Mapping('id', int),
                    Mapping('name', str),
-                   Mapping('mybool', mybool)]
+                   Mapping('bool', bool)]
         self.assertFalse(validate_items(items, mapping))
 
     def test_parse_items(self):
-        from csv2json.main import parse_items, mybool, Mapping
+        from csv2json.main import parse_items
+        from csv2json.mapping import Mapping
+        from csv2json.types import bool
+
         items = ['1', 'some string', 'N']
         mapping = [Mapping('id', int),
                    Mapping('name', str),
-                   Mapping('mybool', mybool)]
-        self.assertDictEqual({'id': 1, 'name': 'some string', 'mybool': False},
+                   Mapping('bool', bool)]
+        self.assertDictEqual({'id': 1, 'name': 'some string', 'bool': False},
                              parse_items(items, mapping))
